68c974e5a9d63358bc0608f64f610637bb7f8d1a
neotom01 <orociotomas02@gmail.com>
2024-04-24 10:35:18 -0600

    actualización de estilo en ejercicio

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,20 @@
-document.oncontextmenu = function(){return false}
+document.oncontextmenu = function(){return false} /*bloqueo menu contextual*/
+var n = 0;
+var min = 0;
+var l = document.getElementById("timequiz");/*ajustes para presentar tiempo*/
+window.setInterval(function(){ /*funcion presentacion tiempo*/
+  
+   if (n==59) {min++; n=0;} /*convierte segundos a minutos*/
+   if (min < 10 && n < 10 ) {l.innerHTML = '0'+min + ':0' +n;}
+   else if (min < 10 && n >= 10 ) {
+   	l.innerHTML = '0'+min + ':' +n;
+   }else if (min > 10 && n < 10 ) {
+   	l.innerHTML = min + ':0' +n;
+   }else if (min => 10 && n >= 10 ) {
+   	l.innerHTML = min + ':' +n;
+   }
+/*condicion en caso de que sobrepase un tiempo dado*/
+   if (min == 3) { l.style.color = 'red';}
+   
+  n++; //aumento de contador segundos
+},1000);
