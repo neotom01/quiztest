f1ef73d7095f81af02055cc4f09601a74289f534
neotom01 <orociotomas02@gmail.com>
2024-04-24 12:42:04 -0600

    estilos web 3

--- js/funciones.js
+++ js/funciones.js
@@ -1,20 +1,21 @@
 document.oncontextmenu = function(){return false} /*bloqueo menu contextual*/
 var n = 0;
 var min = 0;
-var l = document.getElementById("timequiz");/*ajustes para presentar tiempo*/
+var display = document.getElementById("timequiz");/*ajustes para presentar tiempo*/
 window.setInterval(function(){ /*funcion presentacion tiempo*/
   
    if (n==59) {min++; n=0;} /*convierte segundos a minutos*/
-   if (min < 10 && n < 10 ) {l.innerHTML = '0'+min + ':0' +n;}
-   else if (min < 10 && n >= 10 ) {
-   	l.innerHTML = '0'+min + ':' +n;
-   }else if (min > 10 && n < 10 ) {
-   	l.innerHTML = min + ':0' +n;
-   }else if (min => 10 && n >= 10 ) {
-   	l.innerHTML = min + ':' +n;
+   if (min <= 9 && n <= 9 ) {display.innerHTML = '0'+min + ':0' +n;}
+   else if (min <= 9 && n > 9 ) {
+   	display.innerHTML = '0'+min + ':' +n;
+   }else if (min > 9 && n <= 9 ) {
+   	display.innerHTML = min + ':0' +n;
+   }else if (min > 9 && n > 9 ) {
+   	display.innerHTML = min + ':' +n;
    }
 /*condicion en caso de que sobrepase un tiempo dado*/
-   if (min == 3) { l.style.color = 'red';}
+   if (min == 3) { display.style.color = 'orange';}
+   if (min > 10) { display.style.color = '#F75858';}
    
   n++; //aumento de contador segundos
 },1000);
